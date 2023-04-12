3962fb606ff55b904a7a05c34e5bd6463fb62f60
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-04-12 23:20:33 +0430

    make footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  const getCurrentYear = new Date().getFullYear();
+  return (
+    <footer className=' text-center w-full bg-gray-400 text-blue-900'>
+      <p className=' '>
+        parham mosadeqzadeh |{' '}
+        <span className='text-blue-700'>front-end developer</span>{' '}
+      </p>
+      <p className='block text-black'>&copy;{getCurrentYear} </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
@@ -8,6 +9,7 @@ const Layout = ({ children }) => {
       <main className='w-full mx-auto my-2 flex items-center  justify-center flex-col '>
         {children}
       </main>
+      <Footer />
     </>
   );
 };
