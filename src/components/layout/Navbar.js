c7cf30117af6784c742f63c9c43579156707326e
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-04-14 18:53:33 +0430

    add home item to navbar to be rendered

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { HiMenu } from 'react-icons/hi';
 
-const navItems = ['blogs', 'aboutUs'];
+const navItems = ['home', 'blogs', 'aboutUs'];
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -39,7 +39,15 @@ const Navbar = () => {
               className={`cursor-pointer mx-2 capitalize py-3 sm:p-0 hover:text-slate-200 z-50 `}
               key={item}
             >
-              <Link href={`/${item.toLocaleLowerCase()}`}>{item}</Link>
+              <Link
+                href={`/${
+                  item.toLocaleLowerCase() !== 'home'
+                    ? item.toLocaleLowerCase()
+                    : ''
+                }`}
+              >
+                {item}
+              </Link>
             </li>
           ))}
         </ul>
@@ -49,5 +57,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
- 
