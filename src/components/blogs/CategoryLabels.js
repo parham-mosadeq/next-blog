97b4382421d0a5d3f0d5a93167da1f429dda64dc
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-04-14 21:59:29 +0430

    finish cats

--- src/components/blogs/CategoryLabels.js
+++ src/components/blogs/CategoryLabels.js
@@ -2,16 +2,9 @@ import Link from 'next/link';
 import React from 'react';
 
 const CategoryLabels = ({ children }) => {
-  const keys = {
-    Python: 'green',
-    JavaScript: 'yellow',
-    PHP: 'gray',
-    CSS: 'blue',
-    Ruby: 'red',
-  };
   return (
     <div
-      className={`bg-${keys[children]}-500 px-2 py-1 text-gray-50 rounded-md`}
+      className={`bg-purple-500 px-2 py-1 text-gray-50 rounded-md`}
       // className={`bg-blue-500 px-2 py-1 text-gray-50 rounded-md`}
     >
       <Link href={`/blogs/category/${children.toLowerCase()}`}>{children}</Link>
--- src/pages/blogs/page/[page_index].js
+++ src/pages/blogs/page/[page_index].js
@@ -3,21 +3,23 @@ import PaginateHandler from '@/components/blogs/PaginateHandler';
 import Post from '@/components/post/Post';
 import { getAllPosts, getFiles, sortByDate } from '../../../../lib';
 import { POST_PER_PAGE } from '../../../../config';
+
 const BlogPage = ({ posts, numPages, currentPage }) => {
   return (
     <Layout>
-      <article className='grid  items-center justify-center '>
-        <div>
+      <article className='flex items-center justify-center '>
+        <article className='w-full'>
           <h1 className='capitalize text-3xl text-center my-2 border-b-2'>
             all posts
           </h1>
-        </div>
-        <section className='grid w-full min-h-screen  grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
-          {posts.map((post) => {
-            return <Post key={post.slug} post={post} />;
-          })}
-        </section>
-        <PaginateHandler numPages={numPages} currentPage={currentPage} />
+
+          <section className='grid w-full min-h-screen  content-center  grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+            {posts.map((post) => {
+              return <Post key={post.slug} post={post} />;
+            })}
+          </section>
+          <PaginateHandler numPages={numPages} currentPage={currentPage} />
+        </article>
       </article>
     </Layout>
   );
@@ -47,11 +49,13 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+  // checking for page number in url & see if it's
+  // on page 1 or others.
+  // if not the default is page 1
   const page = parseInt((params && params.page_index) || 1);
 
   const posts = getAllPosts();
   const files = getFiles();
-
   const numPages = Math.ceil(files.length / POST_PER_PAGE);
   const pageIndex = page - 1;
 
