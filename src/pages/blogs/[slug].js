66110b001a61a3dc7fac5e63a09dd6e0216cc441
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-04-14 17:17:20 +0430

    create and debug blog pages

---
+++ src/pages/blogs/[slug].js
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout/Layout";
+
+const PostPage = () => {
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+};
+
+export default PostPage;
--- src/pages/blogs/page/[page_index].js
+++ src/pages/blogs/page/[page_index].js
@@ -1,15 +1,14 @@
 import Layout from '@/components/layout/Layout';
-
-import { getAllPosts, sortByDate } from '../../lib';
+import { getAllPosts, sortByDate } from '../../../../lib';
 import Post from '@/components/post/Post';
 
-const HomePage = ({ posts }) => {
+const BlogPage = ({ posts }) => {
   return (
     <Layout>
       <article className='grid  items-center justify-center '>
         <div>
           <h1 className='capitalize text-3xl text-center my-2 border-b-2'>
-            latest posts
+            all posts
           </h1>
         </div>
         <section className='grid w-full min-h-screen  grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
@@ -22,14 +21,14 @@ const HomePage = ({ posts }) => {
   );
 };
 
-export default HomePage;
+export default BlogPage;
 
 export async function getStaticProps() {
   const posts = getAllPosts();
 
   return {
     props: {
-      posts: posts.sort(sortByDate).slice(0, 5),
+      posts: posts.sort(sortByDate),
     },
   };
 }
