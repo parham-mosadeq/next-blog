5952d61636a70650bae66e51ea79490c70398442
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-04-14 20:58:00 +0430

    align items

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   };
   return (
     <header
-      className={` w-full h-10  sm:h-12  z-40 bg-blue-700 flex justify-around items-center text-white`}
+      className={` w-full h-10  sm:h-12  z-40 bg-blue-700 flex items-center justify-around  text-white`}
     >
       {/* logo start */}
       <div className='mx-2 text-md w-full'>
@@ -29,11 +29,11 @@ const Navbar = () => {
       </span>
 
       <nav
-        className={`absolute top-0 right-0 h-screen w-1/2 sm:w-fit sm:h-10 sm:relative  bg-blue-600 sm:bg-inherit  sm:flex z-0 ${
+        className={`absolute top-0 right-0 h-screen sm:justify-center w-1/2 sm:w-fit sm:h-10 sm:relative  bg-blue-600 sm:bg-inherit  sm:flex z-0 ${
           isOpen ? 'block  ' : 'hidden'
         }  `}
       >
-        <ul className='sm:flex  text-center my-10 sm:my-0 border-t-2 sm:border-t-0 sm:mx-2 sm:px-2'>
+        <ul className=' sm:flex sm:items-center sm:justify-center my-10 sm:my-0 border-t-2 sm:border-t-0 sm:mx-2 sm:px-2'>
           {navItems.map((item) => (
             <li
               className={`cursor-pointer mx-2 capitalize py-3 sm:p-0 hover:text-slate-200 z-50 `}
