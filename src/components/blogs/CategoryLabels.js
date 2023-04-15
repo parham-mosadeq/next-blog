530fbcf7198cb86ba6115ebad9348783c2b0a933
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-04-15 16:43:50 +0430

    debug catsLabel

--- src/components/blogs/CategoryLabels.js
+++ src/components/blogs/CategoryLabels.js
@@ -4,8 +4,7 @@ import React from 'react';
 const CategoryLabels = ({ children }) => {
   return (
     <div
-      className={`bg-purple-500 px-2 py-1 text-gray-50 rounded-md`}
-      // className={`bg-blue-500 px-2 py-1 text-gray-50 rounded-md`}
+      className={`bg-purple-500 px-2 py-1 h-fit text-gray-50 rounded-md`}
     >
       <Link href={`/blogs/category/${children.toLowerCase()}`}>{children}</Link>
     </div>
